c96d81abc8c1dd0e71b9105182632b2c77c0ee51
Craig White <craig.white@and.digital>
2020-05-13 21:34:48 +0100

    Readme and basic mux setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Go-API-Learn
+
+Run api with go build
+
+then run ./go-rest-api
+
+port 8080
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+//func main is just a function name but when used with package main,
+// it serves as the application entry point.
